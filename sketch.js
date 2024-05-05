06b032a2d0d437d05b5285bdec22924ca4118a59
lyw93 <150638962+lyw93@users.noreply.github.com>
2024-05-05 20:04:06 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+var radioElement
 var captureGraphics
 var capture_width = 640
 var capture_height = 480
@@ -9,12 +10,25 @@ function setup() {
  captureGraphics = createGraphics(capture_width,capture_height)
  captureGraphics.translate(capture_width,0)
  captureGraphics.scale(-1,1)
-capture.hide()
+ capture.hide()
+ //----旋鈕介面
+ radioElement = createRadio();
+ radioElement.position(width/2-300,20)
+ radioElement.option("方塊")
+ radioElement.option("圓圈")
+ radioElement.option("亮度")
+ radioElement.option("紅底")
+ radioElement.option("文字")
+ radioElement.style("color","#fff")
+ radioElement.style("font-size","30px")
+
 }
 
 function draw() {
-  background(220);
+  background(0);
   noStroke()
+  span = 5+map(mouseX,0,width,0,20)
+  //span=15
   push()
     translate(width/2-capture_width/2, height/2-capture_height/2)//把原點移動(width/2-160, height/2-120)
     captureGraphics.image(capture,0, 0)//在(0,0)
@@ -22,8 +36,41 @@ function draw() {
        for(var y = 0;y<captureGraphics.height;y=y+span){
         var pixel = captureGraphics.get(x,y)
         fill(pixel)
-        rect(x,y,span)
+        if(radioElement.value()=="方塊" || radioElement.value()=="" ){
+          rect(x,y,span)
+        }
+        if(radioElement.value()=="圓圈"){
+          ellipse(x,y,span)
+        }
+        if(radioElement.value()=="亮度"){
+          bk = (pixel[0]+pixel[1]+pixel[2])/3
+          fill(bk)
+         // ellipse(x,y,span) //span代表圓圈直徑
+          ellipse(x,y,span*map(bk,0,255,0,1)) //透過map指令，bk在0~255間，依照比例設定0~1間數字
+        }
+        if(radioElement.value()=="紅底"){
+          colorMode(HSB)
+          
+          fill(pixel[0],80,80)
+          //rotate(pixel[0]/100)
+          rectMode(CENTER)
+          rect(x,y,span*0.6)
+         // ellipse(x,y,span) //span代表圓圈直徑
+         fill(0) 
+         ellipse(x,y,10)
       }
+      if(radioElement.value()=="文字"){
+        const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+        let txt = "一二三四五田雷電龕龘"
+        bk = (pixel[0]+pixel[1]+pixel[2])/3
+        let bkId = int(map(bk, 0, 255, 9, 0))
+        textSize(span)
+        textStyle(BOLD)
+        text(txt[bkId],x,y)
+ 
+    }
+    }
     }
     pop()
 }
+
